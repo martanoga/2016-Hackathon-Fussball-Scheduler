eb8c0b20940c472b3ffa987c5736a9e51fc21bf7
Marta Noga <marta.noga@gmail.com>
2016-07-20 10:47:49 +0200

    added empty file for credentials

---
+++ credentials.empty.js
@@ -0,0 +1,11 @@
+// white listed keys 
+var credentials = {
+    CLIENT_ID: '',
+    CLIENT_SECRET: '',
+    REDIRECT_URL: '',
+    // Notifications channel API keys (private/one per company)
+    SUBSCRIBE_KEY: '',
+    CIPHER_KEY: ''
+}
+
+modeule.exports = credentials;
